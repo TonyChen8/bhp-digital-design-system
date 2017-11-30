8695cc314082b9fc8b6f019859cb2a4d4d51278d
Michael Bertolacci <m.bertolacci@gmail.com>
2017-11-30 11:51:55 +0800

    Fix gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: 'components',
-        path: '${__dirname}/src/components/',
+        path: `${__dirname}/src/components/`,
       },
     },
     {
